1c13cd71e846b7d1a92c6e13dbad02b238adbe57
Lauren Pappas <lauren.pappas@organic.ly>
2022-04-26 13:36:07 -0700

    template tagging

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,3 +1,51 @@
+from random import choices
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+    
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+    
+    def __str__(self) -> str:
+        return f"{self.name} {self.category}"
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f"{self.customer} {self.product}"
+    
